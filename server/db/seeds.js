3da0d1e82298999af30ececd36f6c0921955ece5
Jamie <jamiefogartykk@gmail.com>
2022-08-19 14:22:34 +0100

    create router added

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -1,9 +1,41 @@
-import stocksToInsert from '/getStocks.js'
+const fetch = require('node-fetch')
 
-const stocksToSeed = await stocksToInsert
-console.log(stocksToSeed);
+const getStocks = function(symbol) {
+    return fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbol}&outputsize=full&apikey=BU5R6SXCHWVB2NP0`)
+        .then(res => res.json())
+        .then(result => result["Time Series (Daily)"])
+}
+
+const unpackStocks = async function(symbol) {
+    const stocks = await getStocks(symbol);
+
+    const stockEntries = Object.entries(stocks)
+   
+    const stockObjects = stockEntries.map((entry)=>{
+        return {
+            date: entry[0],
+            info: entry[1]
+        }
+
+    })
+
+    return stockObjects
+}
 
 use stocks
 db.dropDatabase();
 
-db.IBMDaily.insertMany(stocksToInsert);
+const waitingForFetch = async function() {
+    const IBMStocksToInsert = await unpackStocks('IBM')
+    const appleStocksToInsert = await unpackStocks('AAPL')
+    const googleStocksToInsert = await unpackStocks('GOOGL')
+    const teslaStocksToInsert = await unpackStocks('TSLA')
+    const amazonStocksToInsert = await unpackStocks('AMZN')
+    db.IBMDaily.insertMany(IBMStocksToInsert);
+    db.AAPLDaily.insertMany(appleStocksToInsert);
+    db.GOOGLDaily.insertMany(googleStocksToInsert);
+    db.TSLADaily.insertMany(teslaStocksToInsert);
+    db.AMZNDaily.insertMany(amazonStocksToInsert);
+}
+
+waitingForFetch();
