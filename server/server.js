71c3c1582a47072ba5690254dc2ad5acb371c7c9
Andrew Scott Wise <andrewswise2795@gmail.com>
2022-08-19 14:21:44 +0100

    server file built

--- server/server.js
+++ server/server.js
@@ -12,9 +12,21 @@ app.use(express.json());
 MongoClient.connect('mongodb://127.0.0.1:27017', { useUnifiedTopology: true })
   .then((client) => {
     const db = client.db('');
-    const bookingsCollection = db.collection('bookings');
-    const bookingsRouter = createRouter(bookingsCollection);
-    app.use('/api/bookings', bookingsRouter);
+    const IBMCollection = db.collection('IBMDaily');
+    const AAPLCollection = db.collection('AAPLDaily');
+    const GOOGLCollection = db.collection('GOOGLDaily');
+    const TSLACollection = db.collection('TSLADaily');
+    const AMZNCollection = db.collection('AMZNDaily');
+    const IBMRouter = createRouter(IBMCollection);
+    const AAPLRouter = createRouter(AAPLCollection);
+    const GOOGLRouter = createRouter(GOOGLCollection);
+    const TSLARouter = createRouter(TSLACollection);
+    const AMZNRouter = createRouter(AMZNCollection);
+    app.use('/api/IBM', IBMRouter);
+    app.use('/api/AAPL', AAPLRouter);
+    app.use('/api/GOOGL', GOOGLRouter);
+    app.use('/api/TSLA', TSLARouter);
+    app.use('/api/AMZN', AMZNRouter);
   })
   .catch(console.err);
 
