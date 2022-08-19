0c1d83f27210daab43a26ead8b8a2aa11bced42b
Jamie <jamiefogartykk@gmail.com>
2022-08-19 13:42:53 +0100

    API updated

--- server/db/getStocks.js
+++ server/db/getStocks.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch')
 
 const getStocks = function() {
-    return fetch("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&interval=5min&apikey=BU5R6SXCHWVB2NP0")
+    return fetch("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&outputsize=full&apikey=BU5R6SXCHWVB2NP0")
         .then(res => res.json())
         .then(result => result["Time Series (Daily)"])
 }
