4cfd5bfbe5facf5a1bc49ff0487d608105f9ad8b
Andrew Scott Wise <andrewswise2795@gmail.com>
2022-08-22 14:29:16 +0100

    added portfolio router and seeded db

--- server/server.js
+++ server/server.js
@@ -12,16 +12,19 @@ app.use(express.json());
 MongoClient.connect('mongodb://127.0.0.1:27017', { useUnifiedTopology: true })
   .then((client) => {
     const db = client.db('stocks');
+    const portfolioCollection = db.collection('portfolio');
     const IBMCollection = db.collection('IBMDaily');
     const AAPLCollection = db.collection('AAPLDaily');
     const GOOGLCollection = db.collection('GOOGLDaily');
     const TSLACollection = db.collection('TSLADaily');
     const AMZNCollection = db.collection('AMZNDaily');
+    const portfolioRouter = createRouter(portfolioCollection);
     const IBMRouter = createRouter(IBMCollection);
     const AAPLRouter = createRouter(AAPLCollection);
     const GOOGLRouter = createRouter(GOOGLCollection);
     const TSLARouter = createRouter(TSLACollection);
     const AMZNRouter = createRouter(AMZNCollection);
+    app.use('/api/portfolio', portfolioRouter)
     app.use('/api/IBMDaily', IBMRouter);
     app.use('/api/AAPLDaily', AAPLRouter);
     app.use('/api/GOOGLDaily', GOOGLRouter);
