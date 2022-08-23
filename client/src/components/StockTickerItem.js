dfcf4f18018784e51939715e2aea951292955d4d
Jamie <jamiefogartykk@gmail.com>
2022-08-23 11:36:11 +0100

    updated ticker colours on rise/fall

--- client/src/components/StockTickerItem.js
+++ client/src/components/StockTickerItem.js
@@ -3,13 +3,36 @@ import styled from 'styled-components'
 const StockTickerItem = ({name, value, percentage}) => {
     return(
         <Item className={percentage}>
-            {name}: ${value} ({percentage}%)
+            <Name>{name}: </Name> 
+            <Value>${value} </Value> 
+            { (percentage > 0) ? <PercentagePositive>({percentage}%)</PercentagePositive>: 
+            <PercentageNegative>({percentage}%)</PercentageNegative>}
         </Item>
     )
 }
 
 const Item = styled.div`
-    display: flex
+    display: flex;
+    margin-right: 10px 
+`
+
+const Name = styled.b`
+    margin-right: 5px 
+
+`
+const Value = styled.span`
+    margin-right: 5px 
+
+`
+const PercentagePositive = styled.span`
+    margin-right: 5px;
+    color: green;
+
+`
+const PercentageNegative = styled.span`
+    margin-right: 5px;
+    color: red;
+
 `
 
 export default StockTickerItem
